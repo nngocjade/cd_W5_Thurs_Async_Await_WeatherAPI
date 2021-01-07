3ab00b02f1ff56738872b02445e5c944d46f237e
nngocjade <nngocjade@gmail.com>
2021-01-07 11:44:54 +0700

    added MDN geolocation API and used state to set and save longitude and latitude cordinates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // states
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+
+  useEffect(() => {
+    const success = (position) => {
+      console.log(position);
+      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude);
+    };
+    const error = (error) => {
+      console.log(error);
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>longitude: {longitude}</h2>
+      <h2>latitude: {latitude}</h2>
     </div>
   );
 }
